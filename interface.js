627e57eb819ae379f8652af4352a3eeb9ff73b24
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-14 16:22:30 +0100

    Adds current temperature for London

--- interface.js
+++ interface.js
@@ -34,6 +34,10 @@ $(document).ready(function() {
     updateTemperature()
   });
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
